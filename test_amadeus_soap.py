2d4b269cc95314a1d4987f879f142314039cd07f
Emaad Ahmed Manzoor <emaadmanzoor@gmail.com>
2018-01-10 21:53:31 -0500

    fix authentication

--- test_amadeus_soap.py
+++ test_amadeus_soap.py
@@ -9,28 +9,39 @@ if __name__ == "__main__":
     client = zeep.Client(wsdl=WSDL1)
 
     # authenticate
-    resp = client.service.Security_Authenticate(userIdentifier={
-            "originatorTypeCode": "U",
-            "originator": USERNAME
-            },
+    resp = client.service.Security_Authenticate(
+            userIdentifier={
+                "originIdentification": {
+                    "sourceOffice": SOURCEOFFICE
+                    },
+                "originatorTypeCode": "U",
+                "originator": USERNAME
+                },
             passwordInfo={
                 "dataLength": str(len(PASSWORD)),
                 "dataType": "E",
                 "binaryData": base64.b64encode(PASSWORD)
+                },
+            dutyCode={
+                "dutyCodeDetails" : {
+                    "referenceQualifier": "DUT",
+                    "referenceIdentifier": "SU"
+                    }
                 }
             )
-    print resp
-        
+    session = resp["header"]["session"]
+    headers = {"session": session}
+
     # JBU 1012: JetBlue Chicago to Boston
     print client.service.Air_FlightInfo(generalFlightInfo={
             "companyDetails": {
-                "marketingCompany": "JBU"
+                "marketingCompany": "B6" # IATA airline code
                 },
             "flightIdentification": {
                 "flightNumber": "1012"
                 },
             "flightDate": {
-                "departureDate": "011118"
+                "departureDate": "110118" # ddmmyy
                 },
             "boardPointDetails": {
                 "trueLocationId": "ORD"
@@ -38,6 +49,6 @@ if __name__ == "__main__":
             "offPointDetails": {
                 "trueLocationId": "BOS"
                 }
-            })
+            }, _soapheaders=headers)
 
-    client.service.Security_SignOut()
+    client.service.Security_SignOut(_soapheaders=headers)
